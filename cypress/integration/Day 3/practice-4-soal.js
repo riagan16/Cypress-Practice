21796d06d99de0ef3eab709c932dae5075a2cf40
gandaria16 <gandariaganda@gmail.com>
2021-08-25 21:17:47 +0700

    edit assertion have.value

--- cypress/integration/Day 3/practice-4-soal.js
+++ cypress/integration/Day 3/practice-4-soal.js
@@ -1,5 +1,5 @@
 describe('kumpulan test case', function () {
-    it('tc-1', function () {
+    /* it('tc-1', function () {
         cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
         cy.get('#et_pb_contact_name_0').type('Tes Nama').should('have.value', 'Tes Nama')
         cy.get('#et_pb_contact_email_0').type('initesemail@email.com').should('have.value', 'initesemail@email.com')
@@ -15,14 +15,14 @@ describe('kumpulan test case', function () {
         cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
         cy.get('[value="Bike"]').check().should('be.checked')
         cy.get('[value="Car"]').check().should('be.checked')
-    })
+    }) */
 
     it('tc-4', function () {
         cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
-        cy.get('select').select('Audi').should('be.selected','audi')
-    })
+        cy.get('select').select('Audi').should('have.value','audi')
+    }) 
 
-    it('tc-4', function () {
+    it('tc-5', function () {
         cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
         cy.get('.et_pb_cta_0 > .et_pb_button_wrapper > .et_pb_button').click()
         cy.get('.entry-title').should('have.text', 'Button success')
